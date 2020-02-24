90dc002dfd3fdd0a8556bac11cf67c1850c0e172
sebastianslutzky <sebastianslutzky@gmail.com>
2020-02-24 22:17:36 +0000

    Introducing LayoutObject

---
+++ rob/Pages/ObjectView/LayoutGrid.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+using rob.API.ApacheISISApi;
+
+namespace rob.Pages.ObjectView{
+    public partial class ObjectView
+    {
+        [Parameter]
+        public IsisSingleObject Context { get; set; }
+
+
+    
+    }
+}
